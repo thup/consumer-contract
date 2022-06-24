8231b50a96d83e91be5f0a9f3683a3dd6ded503f
tlibn <1039085737@qq.com>
2022-06-24 17:17:36 +0800

    Initial commit

---
+++ src/main/java/com/hogwarts/consumercontract/ConsumerContractApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.consumercontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerContractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hogwarts/consumercontract/ConsumerContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogwarts.consumercontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
